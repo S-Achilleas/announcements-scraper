7f26d6f29c115f218b21833b5541f87c18d5f7db
Achilleas <sarraxilleas@gmail.com>
2024-10-01 21:29:42 +0300

    added logging to check the crontab

--- scraper.py
+++ scraper.py
@@ -2,6 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 import pync
 import datetime
+import logging
+
+# Set up logging
+logging.basicConfig(filename='/Users/axilleas/Desktop/scraping/scraper.log', level=logging.INFO, format='%(asctime)s - %(message)s')
 
 # Dictionary to map Greek month names to month numbers
 months = {
@@ -27,9 +31,8 @@ def date(dt):
     return today == article_date
 
 def get_articles():
-
     headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'
     }
     page = requests.get("https://www.unipi.gr/category/anakoinoseis-tmimatos-oikonomikis-epistimis/", headers=headers)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -39,15 +42,16 @@ def get_articles():
         date_str = article.find("span", class_="elementor-post-date").text.strip()
         if ('Αναβολή' in article.h2.text.strip() or "Πρόγραμμα" in article.h2.text.strip()) and date(date_str):
             titles = article.h2.text.strip()
-            print(titles)
             link = article.a['href']
-            print(link + '\n')
+            logging.info(f"Title: {titles}, Link: {link}")
             pync.notify(
                 title="Νέα ανακοίνωση",
                 message=titles,
                 open=link,
                 timeout=15
             )
-    
+
 if __name__ == "__main__":
+    logging.info("Script started")
     get_articles()
+    logging.info("Script finished")
