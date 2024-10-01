2e3420fe22f648532d49d5e50700454b17b44197
Achilleas <sarraxilleas@gmail.com>
2024-10-01 11:21:38 +0300

    accesed the website

---
+++ scraper.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'
+}
+page = requests.get("https://www.unipi.gr/category/anakoinoseis-tmimatos-oikonomikis-epistimis/", headers=headers)
+soup = BeautifulSoup(page.content, 'html.parser')
