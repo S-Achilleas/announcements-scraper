589eef93469c823775e30baaa995508405b07441
Achilleas <sarraxilleas@gmail.com>
2024-10-01 14:56:07 +0300

    added the notifications

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pync
 
 headers = {
 	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'
@@ -10,7 +11,14 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 articles = soup.find_all('article')
 for article in articles:
-    titles = article.h2.text.strip()
-    print(titles)
-    link = article.a['href']
-    print(link + '\n')
+    if 'Αναβολή' in article.h2.text.strip() or "Πρόγραμμα" in article.h2.text.strip():
+        titles = article.h2.text.strip()
+        print(titles)
+        link = article.a['href']
+        print(link + '\n')
+        pync.notify(
+            title = "Νέα ανακοίνωση",
+            message = titles,
+            open = link,
+            timeout=15
+        )
