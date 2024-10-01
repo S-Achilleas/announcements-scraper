0fabfa388c5bdc6a7bc8d48d739bccded814718d
Achilleas <sarraxilleas@gmail.com>
2024-10-01 21:14:12 +0300

    organized the code into functions

--- scraper.py
+++ scraper.py
@@ -26,23 +26,28 @@ def date(dt):
     article_date = datetime.date(int(year), month, int(day))
     return today == article_date
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'
-}
-page = requests.get("https://www.unipi.gr/category/anakoinoseis-tmimatos-oikonomikis-epistimis/", headers=headers)
-soup = BeautifulSoup(page.content, 'html.parser')
+def get_articles():
 
-articles = soup.find_all('article')
-for article in articles:
-    date_str = article.find("span", class_="elementor-post-date").text.strip()
-    if ('Αναβολή' in article.h2.text.strip() or "Πρόγραμμα" in article.h2.text.strip()) and date(date_str):
-        titles = article.h2.text.strip()
-        print(titles)
-        link = article.a['href']
-        print(link + '\n')
-        pync.notify(
-            title="Νέα ανακοίνωση",
-            message=titles,
-            open=link,
-            timeout=15
-        )
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'
+    }
+    page = requests.get("https://www.unipi.gr/category/anakoinoseis-tmimatos-oikonomikis-epistimis/", headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    articles = soup.find_all('article')
+    for article in articles:
+        date_str = article.find("span", class_="elementor-post-date").text.strip()
+        if ('Αναβολή' in article.h2.text.strip() or "Πρόγραμμα" in article.h2.text.strip()) and date(date_str):
+            titles = article.h2.text.strip()
+            print(titles)
+            link = article.a['href']
+            print(link + '\n')
+            pync.notify(
+                title="Νέα ανακοίνωση",
+                message=titles,
+                open=link,
+                timeout=15
+            )
+    
+if __name__ == "__main__":
+    get_articles()
